ae5fa5669dbbf02f73cb396d09f8ba686e20f8fb
BrianJacobo <jacobo.brian.fuentes@outlook.com>
2022-07-14 22:56:34 -0500

    memorama de numeros

---
+++ tic-tac-toe-frutas/index.js
@@ -0,0 +1,115 @@
+// Inicialización de variables
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let firstResult = null;
+let secondResult = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 59;
+let timerInicial = 59;
+let tiempoRegresivoId = null;
+
+let winAudio = new Audio('./sonidos/ganar.wav')
+let loseAudio = new Audio('./sonidos/perder.wav')
+
+// Apuntando a documento HTML
+let mostrarMovimientos = document.getElementById("movimientos");
+let mostrarAciertos = document.getElementById("aciertos");
+let mostrarTiempo = document.getElementById("t-restante");
+
+let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+numbers.sort(() => Math.random() - 0.3);
+console.log(numbers);
+
+function contarTiempo() {
+  tiempoRegresivoId = setInterval(
+    () => {
+      timer--;
+      mostrarTiempo.innerHTML = `Tiempo restante: ${timer} segundos`;
+      if (timer < 0) {
+        clearInterval(tiempoRegresivoId);
+        bloquearTarjetas();
+        loseAudio.play();
+      }
+    },
+    1000,
+    timer
+  );
+}
+
+function bloquearTarjetas() {
+  for (let i = 0; i < 16; i++) {
+    let tarjetaBloqueada = document.getElementById(i);
+    tarjetaBloqueada.innerHTML = numbers[i];
+    tarjetaBloqueada.innerHTML = `<img src="frutas/${numbers[i]}.png" alt="${numbers[i]}">`;
+    tarjetaBloqueada.disabled = true;
+  }
+}
+
+function mostrar(id) {
+  if (temporizador == false) {
+    contarTiempo();
+    temporizador = true;
+  }
+  // tarjeta1 = document.getElementById(id);
+  // firstResult = numbers[id];
+  // tarjeta1.innerHTML = `<img src="img/${firstResult}.png" alt="${firstResult}">`;
+
+  tarjetasDestapadas++;
+  console.log(tarjetasDestapadas);
+
+  if (tarjetasDestapadas === 1) {
+    // Mostrar primer número
+    tarjeta1 = document.getElementById(id);
+    firstResult = numbers[id];
+    // tarjeta1.innerHTML = firstResult;
+    tarjeta1.innerHTML = `<img src="frutas/${firstResult}.png" alt="${firstResult}">`;
+    // Deshabilitar el primer botón
+    tarjeta1.disabled = true;
+  } else if (tarjetasDestapadas === 2) {
+    // Mostrar segundo número
+    tarjeta2 = document.getElementById(id);
+    secondResult = numbers[id];
+    tarjeta2.innerHTML = `<img src="frutas/${secondResult}.png" alt="${secondResult}">`;
+
+    // Deshabilitar el segundo botón
+    tarjeta2.disabled = true;
+
+    // Incrementar movimientos
+    movimientos++;
+    mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+
+    if (firstResult === secondResult) {
+      //   Encerar contador de tarjetas destapadas
+      tarjetasDestapadas = 0;
+
+      // Aumentar aciertos
+      aciertos++;
+      mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+      winAudio.play();
+    }
+
+    if (aciertos === 8) {
+      winAudio.play();
+      clearInterval(tiempoRegresivoId);
+      mostrarAciertos.innerHTML = `Felicidades, has ganado!`;
+      mostrarTiempo.innerHTML = `Gnacias, has ganado en ${
+        timerInicial - timer
+      } segundos`;
+      mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+    }
+  } else {
+    // Mostrar momentaneamente valores y volver a tapar
+    setTimeout(() => {
+      tarjeta1.innerHTML = "";
+      tarjeta2.innerHTML = "";
+      tarjeta1.disabled = false;
+      tarjeta2.disabled = false;
+      tarjetasDestapadas = 0;
+      loseAudio.play();
+    }, 200);
+  }
+}
---
+++ tic-tac-toe/index.js
@@ -0,0 +1,100 @@
+// Inicialización de variables
+let tarjetasDestapadas = 0;
+let tarjeta1 = null;
+let tarjeta2 = null;
+let firstResult = null;
+let secondResult = null;
+let movimientos = 0;
+let aciertos = 0;
+let temporizador = false;
+let timer = 30;
+let timerInicial = 30;
+let tiempoRegresivoId = null;
+
+// Apuntando a documento HTML
+let mostrarMovimientos = document.getElementById("movimientos");
+let mostrarAciertos = document.getElementById("aciertos");
+let mostrarTiempo = document.getElementById("t-restante");
+
+let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+
+numbers.sort(() => Math.random() - 0.3);
+console.log(numbers);
+
+function contarTiempo() {
+  tiempoRegresivoId = setInterval(() => {
+    timer--;
+    mostrarTiempo.innerHTML = `Tiempo restante: ${timer} segundos`;
+    if (timer < 0) {
+      clearInterval(tiempoRegresivoId);
+      bloquearTarjetas();
+    }
+  }, 1000, timer);
+}
+
+function bloquearTarjetas() {
+  for (let i = 0; i < 16; i++) {
+    let tarjetaBloqueada = document.getElementById(i);
+    tarjetaBloqueada.innerHTML = numbers[i];
+    tarjetaBloqueada.disabled = true;
+  }
+}
+
+function mostrar(id) {
+  if (temporizador == false) {
+    contarTiempo();
+    temporizador = true;
+  }
+
+  tarjetasDestapadas++;
+  console.log(tarjetasDestapadas);
+  
+  if (tarjetasDestapadas === 1) {
+    // Mostrar primer número
+    tarjeta1 = document.getElementById(id);
+    firstResult = numbers[id];
+    tarjeta1.innerHTML = firstResult;
+
+    // Deshabilitar el primer botón
+    tarjeta1.disabled = true;
+  } else if (tarjetasDestapadas === 2) {
+    // Mostrar segundo número
+    tarjeta2 = document.getElementById(id);
+    secondResult = numbers[id];
+    tarjeta2.innerHTML = secondResult;
+
+    // Deshabilitar el segundo botón
+    tarjeta2.disabled = true;
+
+    // Incrementar movimientos
+    movimientos++;
+    mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+
+    if (firstResult === secondResult) {
+      //   Encerar contador de tarjetas destapadas
+      tarjetasDestapadas = 0;
+
+      // Aumentar aciertos
+      aciertos++;
+      mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+    }
+
+    if (aciertos === 8) { 
+      clearInterval(tiempoRegresivoId);
+      mostrarAciertos.innerHTML = `Felicidades, has ganado!`;
+      mostrarTiempo.innerHTML = `Gnacias, has ganado en ${
+        timerInicial - timer
+      } segundos`;
+      mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+    }
+  } else {
+    // Mostrar momentaneamente valores y volver a tapar
+    setTimeout(() => {
+      tarjeta1.innerHTML = "";
+      tarjeta2.innerHTML = "";
+      tarjeta1.disabled = false;
+      tarjeta2.disabled = false;
+      tarjetasDestapadas = 0;
+    }, 200);
+  }
+}
